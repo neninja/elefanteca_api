90f2e3e69dcb830d64db0c372622492c62612ed6
Felipe Silva <felipe.dacs@gmail.com>
2022-02-20 00:50:13 -0300

    :cop: abstrai criação de colaborador

--- tests/E2ETestCase.php
+++ tests/E2ETestCase.php
@@ -9,6 +9,20 @@ abstract class E2ETestCase extends LumenTestCase
     protected function jsonComoColaborador(
         string $method, string $ep, array $body = [], $headers = []
     ) {
+        $bodyLoginRequest = $this->criaColaborador()['loginJWT'];
+
+        $access = $this
+            ->json('GET', '/api/auth/login/jwt', $bodyLoginRequest)
+            ->response
+            ->decodeResponseJson();
+
+        $headers['Authorization'] = "Bearer {$access['token']}";
+
+        return $this->json($method, $ep, $body, $headers);
+    }
+
+    protected function criaUsuario($papel)
+    {
         $faker = Faker\Factory::create('pt_BR');
 
         $s = $this->factory(CadastroUsuarioService::class);
@@ -21,21 +35,20 @@ abstract class E2ETestCase extends LumenTestCase
             cpf:    $faker->cpf(false),
             email:  $email,
             senha:  $passw,
-            papel:  Papel::$COLABORADOR
+            papel:  $papel
         );
 
-        $bodyLoginRequest = [
-            'email'     => $email,
-            'password'  => $passw,
+        return [
+            'loginJWT' => [
+                'email'     => $email,
+                'password'  => $passw,
+            ],
+            'usuario' => $u,
         ];
+    }
 
-        $access = $this
-            ->json('GET', '/api/auth/login/jwt', $bodyLoginRequest)
-            ->response
-            ->decodeResponseJson();
-
-        $headers['Authorization'] = "Bearer {$access['token']}";
-
-        return $this->json($method, $ep, $body, $headers);
+    protected function criaColaborador()
+    {
+        return $this->criaUsuario(Papel::$COLABORADOR);
     }
 }
